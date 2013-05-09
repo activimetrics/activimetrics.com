1ec8abcb7f31472a699e0c6d4af5297913cc05f1
Craig Rindt <crindt@gmail.com>
2013-05-08 19:31:09 -0700

    Drop server to nobody on startup

--- server.js
+++ server.js
@@ -19,4 +19,29 @@ app.get("/", function(req, res) {
   res.render('index');
 });
 
-app.listen(port);
+app.listen(port,'0.0.0.0', function(e){
+    if(e){
+        // problems
+        console.log('cannot start server')
+        throw new Error(e)
+    }
+    console.log("Express server listening port" + port);
+    console.log('Current gid: ' + process.getgid());
+    try {
+        process.setgid(65533);
+        console.log('New gid: ' + process.getgid());
+    }
+    catch (err) {
+        console.log('Failed to set gid: ' + err);
+        if(process.getgid() === 0) throw(err);
+    }
+    console.log('Current uid: ' + process.getuid());
+    try {
+        process.setuid(65534);
+        console.log('New uid: ' + process.getuid());
+    }
+    catch (err) {
+        console.log('Failed to set uid: ' + err);
+        if(process.getuid() === 0) throw(err);
+    }
+})
